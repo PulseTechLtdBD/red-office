b6eb61f8987903103faf6a8ab90402960a39b55b
sakib <mhmsakib@gmail.com>
2025-07-08 18:35:57 +0600

    updated all CreateUpdate files

--- resources/js/Pages/Admin/Organizations/CreateUpdate.vue
+++ resources/js/Pages/Admin/Organizations/CreateUpdate.vue
@@ -81,7 +81,7 @@ const createNewOrg = () => {
                                 v-model="form.parent_id"
                                 class="mt-1 block w-full rounded-md shadow-sm border-gray-300 focus:ring-indigo-500 focus:border-indigo-500"
                             >
-                                <option value="">-- Select Parent Organization --</option>
+                                <option value="" disabled>-- Select Parent Organization --</option>
                                 <option 
                                 v-for="organization in props.organizations"
                                 :key="organization.id"
